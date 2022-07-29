f35b34c16bf15dbbbb0248a9164372246d902116
Sergey <s.kurochkin@itfbgroup.ru>
2022-07-29 12:09:23 +0000

    Note

--- src/main/java/App/Rest/ExceptionHandler/RestExceptionHandler.java
+++ src/main/java/App/Rest/ExceptionHandler/RestExceptionHandler.java
@@ -2,13 +2,10 @@ package App.Rest.ExceptionHandler;
 import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.web.util.UrlUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 //--- https://www.javainterviewpoint.com/spring-boot-exception-handling/
 
